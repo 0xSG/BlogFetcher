6c2f78e5b2cf1e4804dc4cdbcc3a9eb202e5bf6d
sooryagangarajk <sooryagangaraj@gmail.com>
2018-01-14 22:37:45 +0530

    Dark Theme added

--- app/src/main/java/tb/sooryagangarajk/com/blogfetcher/PostContents.java
+++ app/src/main/java/tb/sooryagangarajk/com/blogfetcher/PostContents.java
@@ -1,19 +1,22 @@
 package tb.sooryagangarajk.com.blogfetcher;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.ScrollingMovementMethod;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 public class PostContents extends AppCompatActivity {
     public static TextView tView;
     public static TextView contView;
-
+    public static String url;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +24,7 @@ public class PostContents extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_post_contents);
         Intent intent=getIntent();
-        String url = intent.getExtras().getString("url");
+        url = intent.getExtras().getString("url");
         String content = intent.getExtras().getString("content");
         String title = intent.getExtras().getString("title");
         tView=(TextView)findViewById(R.id.tid);
@@ -34,6 +37,14 @@ public class PostContents extends AppCompatActivity {
 
         }
         contView.setMovementMethod(new ScrollingMovementMethod());
+        tView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uriUrl = Uri.parse(url);
+                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+                startActivity(launchBrowser);
+            }
+        });
 
     }
 }
